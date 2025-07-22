b89daab966bcbd768f10df2c2c8bf3a57e151e3a
MolnarHangaBorbala <molnarhanga4@gmail.com>
2025-07-23 00:49:26 +0200

    Add files via upload

---
+++ js/indexscr.js
@@ -0,0 +1,47 @@
+const MultiBtn = document.getElementById("MultiBtn");
+const CompBtn = document.getElementById("CompBtn");
+const playButton = document.getElementById("Playbtn");
+const LoadingText = document.getElementById("loading");
+LoadingText.style.opacity = "0";
+
+MultiBtn.disabled = false;
+CompBtn.disabled = false;
+
+playButton.classList.add("no-link");
+playButton.classList.remove("link");
+playButton.classList.remove("Playbtn");
+
+let btn = 0;
+
+MultiBtn.addEventListener("click", () => {
+    playButton.disabled = false;
+    MultiBtn.classList.add("active");
+    CompBtn.classList.remove("active");
+    playButton.classList.add("link");
+    playButton.classList.remove("no-link");
+    playButton.classList.add("Playbtn");
+    btn = 1;
+});
+
+CompBtn.addEventListener("click", () => {
+    playButton.disabled = false;
+    CompBtn.classList.add("active");
+    MultiBtn.classList.remove("active");
+    playButton.classList.add("link");
+    playButton.classList.remove("no-link");
+    playButton.classList.add("Playbtn");
+    btn = 2;
+});
+
+playButton.addEventListener("click", () => {
+    LoadingText.style.opacity = "1";
+    MultiBtn.disabled = true;
+    CompBtn.disabled = true;
+    setTimeout(() => {
+        if (btn === 1) {
+            window.location.href = "html/multi.html";
+        } else if (btn === 2) {
+            window.location.href = "html/comp.html";
+        }
+    }, 1500);
+});
