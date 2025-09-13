332f8685155e8abb6f3e4c83b9fd1e4d72a7283a
MolnarHangaBorbala <molnarhanga4@gmail.com>
2025-09-13 20:58:12 +0200

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,12 @@
 // ------- FIREBASE LEADERBOARD (Claude) ----------
-
 const firebaseConfig = {
     apiKey: "AIzaSyB51eR9v4KvC8MdwBcJ68IXIdjou1EeXa0",
     authDomain: "planet-merge-game.firebaseapp.com",
+    databaseURL: "https://planet-merge-game-default-rtdb.europe-west1.firebasedatabase.app/",
     projectId: "planet-merge-game",
     storageBucket: "planet-merge-game.firebasestorage.app",
     messagingSenderId: "900875497215",
-    appId: "1:900875497215:web:9edf2c0b3acfa6d9c3a6ca",
-    measurementId: "G-NYP02VYZWK"
+    appId: "1:900875497215:web:9edf2c0b3acfa6d9c3a6ca"
 };
 
 let database;
